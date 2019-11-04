14cd3042735aba4243686625f383e89551761270
Anton Te <te.anton@gmail.com>
2019-11-04 10:27:30 -0800

    Deploy to dev bot

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd build
 make -j7
-ssh mika@tye-pod-dog.mika.global killall echo_bot
-scp echo_bot mika@tye-pod-dog.mika.global:tye-pod-dog/
+ssh mika@tye-pod-dog.mika.global killall echo_bot_dev
+scp echo_bot mika@tye-pod-dog.mika.global:tye-pod-dog-dev/echo_bot_dev
