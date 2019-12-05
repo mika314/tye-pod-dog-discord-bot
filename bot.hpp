347f7a9046032391183938f76d4cecd4943ee465
Mika Pi <mika+github@mika.global>
2019-12-04 21:32:07 -0800

    Respect rate limit

--- bot.hpp
+++ bot.hpp
@@ -1,14 +1,15 @@
 #pragma once
+#include <chrono>
 #include <functional>
 #include <memory>
 #include <nlohmann/json.hpp>
-#include <chrono>
+#include <unordered_set>
 
 namespace Internal
 {
   class Bot;
   struct Timer;
-}
+} // namespace Internal
 
 enum class Handler {
   ChannelCreate,
@@ -28,6 +29,7 @@ enum class Handler {
 class [[nodiscard]] InvokeToken
 {
   friend Internal::Bot;
+
 public:
   InvokeToken();
   ~InvokeToken();
@@ -35,6 +37,16 @@ public:
   InvokeToken(InvokeToken &&);
   InvokeToken &operator=(const InvokeToken &) = delete;
   InvokeToken &operator=(InvokeToken &&);
+  bool operator==(const InvokeToken &token) const;
+
+  class Hash
+  {
+  public:
+    size_t operator()(const InvokeToken &p) const
+    {
+      return std::hash<Internal::Timer *>{}(p.timer.get());
+    }
+  };
 
 private:
   InvokeToken(Internal::Timer &&);
@@ -48,11 +60,12 @@ public:
   ~Bot();
   void run();
   void reg(Handler, std::function<void(Bot &, const nlohmann::json &)>);
-  const nlohmann::json& self() const;
+  const nlohmann::json &self() const;
   void message(const std::string &channelId, const std::string &message);
   InvokeToken invokeAt(std::chrono::system_clock::time_point, std::function<void(Bot &)>);
   InvokeToken invokeFromNow(std::chrono::milliseconds, std::function<void(Bot &)>);
 
 private:
   std::unique_ptr<Internal::Bot> bot;
+  std::unordered_set<InvokeToken, InvokeToken::Hash> tokens;
 };
